c9afb299eaf1f5148db33a76fbfeff9351bf5ee3
davmba <davidmbahaotu@gmail.com>
2022-07-26 00:30:04 +0100

    indentation fixed in js file

--- app.js
+++ app.js
@@ -3,19 +3,19 @@ const navMenu = document.querySelector('.nav');
 const xIcon = document.querySelector('.xmark');
 const navLinks = document.querySelectorAll('.nav-link');
 
-hamburger.addEventListener('click', ()=> {
+hamburger.addEventListener('click', () => {
   hamburger.classList.remove('hamburger');
   navMenu.classList.add('active');
   xIcon.classList.add('nav-open');
 });
 
-xIcon.addEventListener('click', ()=>{
+xIcon.addEventListener('click', () =>{
   xIcon.classList.remove('nav-open');
   navMenu.classList.remove('active');
   hamburger.classList.add('hamburger');
 });
 
-navLinks.forEach((n) => n.addEventListener('click', ()=> {
+navLinks.forEach((n) => n.addEventListener('click', () => {
   navMenu.classList.remove('active');
   xIcon.classList.remove('nav-open');
   hamburger.classList.add('hamburger');
