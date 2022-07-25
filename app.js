5edc63de562264b2642f15e6e8d0427ede4d6a13
davmba <davidmbahaotu@gmail.com>
2022-07-26 00:07:01 +0100

    indentation modified

--- app.js
+++ app.js
@@ -3,20 +3,20 @@ const navMenu = document.querySelector('.nav');
 const xIcon = document.querySelector('.xmark');
 const navLinks = document.querySelectorAll('.nav-link');
 
-hamburger.addEventListener('click', () => {
-    hamburger.classList.remove('hamburger');
-    navMenu.classList.add('active');
-    xIcon.classList.add('nav-open');
+hamburger.addEventListener('click', ()=> {
+  hamburger.classList.remove('hamburger');
+  navMenu.classList.add('active');
+  xIcon.classList.add('nav-open');
 });
 
-xIcon.addEventListener('click', () => {
-    xIcon.classList.remove('nav-open');
-    navMenu.classList.remove('active');
-    hamburger.classList.add('hamburger');
+xIcon.addEventListener('click', ()=>{
+  xIcon.classList.remove('nav-open');
+  navMenu.classList.remove('active');
+  hamburger.classList.add('hamburger');
 });
-  
-navLinks.forEach((n) => n.addEventListener('click', () => {
-    navMenu.classList.remove('active');
-    xIcon.classList.remove('nav-open');
-    hamburger.classList.add('hamburger');
+
+navLinks.forEach((n) => n.addEventListener('click', ()=> {
+  navMenu.classList.remove('active');
+  xIcon.classList.remove('nav-open');
+  hamburger.classList.add('hamburger');
 }));
