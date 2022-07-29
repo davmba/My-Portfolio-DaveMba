8df2bf43630330b129be1c9736aacbc0784e82a6
davmba <davidmbahaotu@gmail.com>
2022-07-29 11:55:37 +0100

    linters indentation modified

--- app.js
+++ app.js
@@ -28,13 +28,12 @@ const showItemInfo = [
     desc: {
       card: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
       desktop: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita.",
-      mobile: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent"
+      mobile: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
     },
     links: {
       liveVersion: 'https://davmba.github.io/My-Portfolio-DaveMba/',
-      liveSource: 'https://github.com/davmba/My-Portfolio-DaveMba.git'
-    }
-    
+      liveSource: 'https://github.com/davmba/My-Portfolio-DaveMba.git',
+    },
   },
   {
     title: 'Multi-Post Stories',
@@ -42,11 +41,11 @@ const showItemInfo = [
     desc: {
       card: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
       desktop: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita.",
-      mobile: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent"
+      mobile: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
     },
     links: {
       liveVersion: 'https://davmba.github.io/My-Portfolio-DaveMba/',
-      liveSource: 'https://github.com/davmba/My-Portfolio-DaveMba.git'
+      liveSource: 'https://github.com/davmba/My-Portfolio-DaveMba.git',
     },
   },
   {
